10cd0ec08c2c0a0137987d7538979c63b8fb24ce
Manoel Benedito Neto <manoel.neto.101@ufrn.edu.br>
2022-06-26 20:11:46 -0400

    All fine v.0.1

--- data-app/app.py
+++ data-app/app.py
@@ -1,8 +1,12 @@
 import os, sys
 import pandas as pd
 import numpy as np
+from datetime import datetime
 import json, time
 from database import insert_firerisks_data
+
+def _print(*args, **kw):
+    print("[%s]" % (datetime.now()), *args, **kw)
 
 #INPE: https://queimadas.dgi.inpe.br/queimadas/dados-abertos/#
 
@@ -10,27 +14,32 @@ COUNTRY='33'
 STATE='13'
 INPE_API_LINK=f'https://queimadas.dgi.inpe.br/api/focos/?pais_id={COUNTRY}&estado_id={STATE}' # pais_id=33(br) e estado_id=13(amazonas)
 
-print('Contacting INPE API...')
-print(f'Country: ({COUNTRY})')
-print(f'State: ({STATE})')
-print(f'API Link: ({INPE_API_LINK})')
+_print("> Start data-app!")
+_print('> Contacting INPE API...')
+_print(f'  Country: ({COUNTRY})')
+_print(f'  State: ({STATE})')
+_print(f'  API Link: ({INPE_API_LINK})')
 
 while (True):
-    print("Sending request...")
+    _print("> Sending request...")
     try:
         df_queimadas = pd.read_json(INPE_API_LINK)
         df_queimadas["geometry"] = df_queimadas["geometry"].map(lambda item: json.dumps(item))
         df_queimadas["properties"] = df_queimadas["properties"].map(lambda item: json.dumps(item))
 
-        print("Data collected: ")
-        print(df_queimadas.head()) # Dataframe first row
-        print(df_queimadas.count()) # Data info
-        print(df_queimadas.columns) # Dataframe columns
+        _print("   Data collected: ")
+        _print(df_queimadas.head()) # Dataframe first row
+        _print(df_queimadas.count()) # Data info
+        _print(df_queimadas.columns) # Dataframe columns
 
         insert_firerisks_data(df_queimadas)
     except Exception as e:
-        print(e)
-    
-    print("Time interval!")
-    time.sleep(600) #86400 (24h)
-    print("End of time interval!".center(40, "#"))
+        _print("> Request failed!")
+        _print(e)
+        _print("> Trying again in 5min...")
+        time.sleep(300)
+        _print("Restart!".center(40, "-"))
+    else:
+        _print("> Time interval!")
+        time.sleep(600) #86400 (24h)
+        _print("End of time interval!".center(40, "#") + "\n")
